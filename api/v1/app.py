3deb8674c240c4380640f3feb6fb346261c54ae0
Heba Omar <jtvonline@yahoo.com>
2023-10-27 14:32:46 +0300

    Updated the README

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-from flask import Flask
-from models import storage
+""" Flask API """
 from os import getenv
+from flask import Flask, jsonify
+from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+""" Creates Flask instant """
+app_host = getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def downtear(self):
+def teardown_flask(exception):
     """ Calls storage.close() """
     storage.close()
 
 
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(404)
+def error_404(error):
+    """ Handles 404 Not Found """
+    return jsonify(error='Not found'), 404
+
+
+if __name__ == '__main__':
+    app_host = getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
