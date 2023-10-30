e71534940897dc2976276edba29d5b14a67481b2
Heba Omar <jtvonline@yahoo.com>
2023-10-30 21:59:16 +0300

    Fix import

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -6,8 +6,8 @@ handles all default RestFul API actions
 from api.v1.views.__init__ import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models import Place
-from models import Amenity
+from models.place import Place
+from models.amenity import Amenity
 from os import getenv
 
 
